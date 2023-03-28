0ac4716820575b47f002bbfe84f8bfb8e2507240
show <d0r5i5z1z8l0e@gmail.com>
2023-03-28 11:18:08 +0800

    cli impl

---
+++ markdown_ed/src/cli_lib/cli_struct.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
