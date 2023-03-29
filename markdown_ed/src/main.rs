d97c282a925d357359b1f8151fd38b0157eea6d8
show <d0r5i5z1z8l0e@gmail.com>
2023-03-29 15:24:46 +0800

    import imp_method and use imp_method function

--- markdown_ed/src/main.rs
+++ markdown_ed/src/main.rs
@@ -1,45 +1,14 @@
 
 mod cli_lib {
-    mod imp_method;
+    pub(crate) mod imp_method;
     pub(crate) mod cli_struct;
+
+    //pub(crate) use imp_method::*;
 }
 
-
-use pulldown_cmark::{Parser, Options, html};
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
+use crate::cli_lib::{cli_struct::Cli, imp_method::file_read};
 use std::path::Path;
 
-use crate::cli_lib::cli_struct::Cli;
-
-
-fn file_read(file_name:&Path) ->std::io::Result<()>{
-    let file = File::open(file_name)?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
-
-
-    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
-    // and we therefore must enable it explicitly.
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    options.insert(Options::ENABLE_TASKLISTS);
-    options.insert(Options::ENABLE_FOOTNOTES);
-    options.insert(Options::ENABLE_TABLES);
-    let parser = Parser::new_ext(&contents, options);
-
-    // Write to String buffer.
-    let mut html_output = String::new();
-    html::push_html(&mut html_output, parser);
-
-
-    println!("orignal: {:?}",contents);
-    println!("html: {:?}",html_output);
-
-    Ok(())
-}
 
 
 fn main() ->std::io::Result<()>{
