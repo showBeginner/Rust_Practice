3830340f621f9dced3461d3e20f38c10a6077ee2
show <d0r5i5z1z8l0e@gmail.com>
2023-04-25 21:02:22 +0800

    implement Queue data struct with Rust lang

--- imp_queue/src/main.rs
+++ imp_queue/src/main.rs
@@ -1,3 +1,36 @@
+use std::collections::VecDeque;
+struct QueueSt<T> {
+    element: VecDeque<T>,
+}
+
+impl<T> QueueSt<T>{
+
+    fn new()->QueueSt<T>{
+        QueueSt{ element: VecDeque::new()}
+    }
+
+    fn pop_element(&mut self){
+        self.element.pop_front();
+    }
+
+    fn push_element(&mut self,temp:T){
+        self.element.push_back(temp);
+    }
+
+    fn top_element(&self) -> Option<&T>{
+        self.element.front()
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut myqueue = QueueSt::<i32>::new();
+
+    myqueue.push_element(1);
+
+    match myqueue.top_element() {
+        Some(x) => println!("Top element: {}",x),
+        None =>println!("Empty queue!"),
+    }
+    myqueue.pop_element();
 }
